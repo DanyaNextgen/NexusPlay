727cb8cd9f216a8f1e28a713637b3da3d47e2523
Danya-web <danilameretov20@gmail.com>
2025-06-23 04:27:19 +0500

    changes

--- app/[locale]/product/[id]/page.tsx
+++ app/[locale]/product/[id]/page.tsx
@@ -2,16 +2,11 @@ import { notFound } from "next/navigation";
 import { getProductById } from "@/app/[locale]/admin/dashboard/actions";
 import ProductClient from "@/components/custom/ProductClient";
 
-export default async function ProductPage({
-    params,
-}: {
-    params: { locale: string; id: string } | Promise<{ locale: string; id: string }>;
-}) {
-    const resolvedParams = await params;
-    const product = await getProductById(resolvedParams.id);
+export default async function ProductPage({ params }: any) {
+    const product = await getProductById(params.id);
     if (!product) return notFound();
-
     return <ProductClient product={product} />;
 }
+
 
 
