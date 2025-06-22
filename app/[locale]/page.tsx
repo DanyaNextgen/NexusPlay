e906c4b08c50ea20c6b8883a4381166929ed5476
Danya-web <danilameretov20@gmail.com>
2025-06-23 03:19:23 +0500

    add SEO

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,6 +1,27 @@
 import SmartSearch from "@/components/custom/SmartSearch";
 import ProductSection from "@/components/custom/ProductSection";
 import { getProducts } from "./admin/dashboard/actions";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "NexusPlay – Открой для себя новые игры",
+	description: "Ищи, открывай и покупай лучшие игры. Наслаждайся качественным геймингом на платформе NexusPlay.",
+	openGraph: {
+		title: "NexusPlay – Твоя игровая платформа",
+		description: "Игры всех жанров, лучшие предложения, новинки и популярные хиты.",
+		url: "https://nexusplay.uz",
+		siteName: "NexusPlay",
+		images: [
+			{
+				url: "/NexusPlay.png",
+				width: 1200,
+				height: 630,
+				alt: "NexusPlay Banner",
+			},
+		],
+		type: "website",
+	},
+};
 
 export default async function HomePage() {
 	const products = await getProducts();
@@ -15,7 +36,7 @@ export default async function HomePage() {
 			<SmartSearch products={products} />
 
 			<div className="max-w-[1200px] mx-auto space-y-10">
-				<ProductSection titleKey="discoverTitle" products={first}/>
+				<ProductSection titleKey="discoverTitle" products={first} />
 				<ProductSection titleKey="popular" products={second} />
 				<ProductSection titleKey="recent" products={third} />
 			</div>
